17252c574adbef194b1854af6f3f452ac2c7f31a
ArmaanMcleod <armaan_mcleod@outlook.com>
2022-03-16 09:56:34 +1100

    Create container in build script

--- terraform-aks-k8s/build.ps1
+++ terraform-aks-k8s/build.ps1
@@ -7,4 +7,6 @@ param (
     [string]$StorageAccountAccessKey
 )
 
+az storage container create -n tfstate --account-name "${StorageAccountName}" --account-key "${StorageAccountAccessKey}"
+
 terraform init -backend-config="storage_account_name=${StorageAccountName}" -backend-config="container_name=tfstate" -backend-config="access_key=${StorageAccountAccessKey}" -backend-config="key=codelab.microsoft.tfstate"
